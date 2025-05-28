37f24317850329b95493cbf28947a5d5ef13b9d7
Angelson007 <angelsonmanana@gmail.com>
2025-05-28 20:44:09 +0300

    Backend Django

---
+++ produits/urls.py
@@ -0,0 +1,9 @@
+# produits/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('produits/', views.liste_produits, name='liste_produits'),
+    path('produits/ajouter/', views.ajouter_produit, name='ajouter_produit'),
+    path('produits/supprimer/<int:id>/', views.supprimer_produit, name='supprimer_produit'),
+]
---
+++ produits/views.py
@@ -0,0 +1,29 @@
+# produits/views.py
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Produit
+from .serializers import ProduitSerializer
+
+@api_view(['GET'])
+def liste_produits(request):
+    produits = Produit.objects.all()
+    serializer = ProduitSerializer(produits, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def ajouter_produit(request):
+    serializer = ProduitSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def supprimer_produit(request, id):
+    try:
+        produit = Produit.objects.get(id=id)
+    except Produit.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    produit.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
