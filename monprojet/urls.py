c27bfe750c63ffa32aea147080a1d6019c1f798f
Angelson007 <angelsonmanana@gmail.com>
2025-05-29 13:00:14 +0300

    ajout de migrate

--- monprojet/urls.py
+++ monprojet/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from .views import home
 
 urlpatterns = [
-    path('', home, name='home'),  # route racine
+    path('', home, name='home'),
     path('api/', include('produits.urls')),
 ]
